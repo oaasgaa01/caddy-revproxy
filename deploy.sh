0f8a1d9696d33ed795f9a5b14fb41ed180d4c861
Olav Aasgaard <olav.aasgaard@siemens-healthineers.com>
2020-11-01 08:59:43 +0100

    Ready to test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,3 @@
 docker build --tag oaasgaa01/caddy-revproxy --no-cache .
 docker stop revproxy
 docker run -d --rm --name revproxy -p 80:80 -p 443:443 -v /site:/srv -v revproxy_data:/data -v revproxy_config:/config oaasgaa01/caddy-revproxy
-
-# docker run -d --rm --name revproxy -v revproxy_data:/data -v revproxy_config -p 80:80 -p 443:443 oaasgaa01/caddy-revproxy
-# docker run -d --rm --name revproxy -p 80:80 -p 443:443 -v /site:/srv -v caddy_data:/data -v caddy_config:/config caddy file-server --domain aasgaards.no
