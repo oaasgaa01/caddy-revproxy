97970733e7c68c502d383bff6f4b1314473082fb
Olav Aasgaard <olav.aasgaard@siemens-healthineers.com>
2020-11-01 09:19:33 +0100

    Changed port on r reverse proxy to 16102

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash    
 docker build --tag oaasgaa01/caddy-revproxy --no-cache .
 docker stop revproxy
-docker run -d --rm --name revproxy -p 80:80 -p 443:443 -v /site:/srv -v revproxy_data:/data -v revproxy_config:/config oaasgaa01/caddy-revproxy
+docker run -d --rm --name revproxy -p 80:80 -p 443:443 -v revproxy_data:/data -v revproxy_config:/config oaasgaa01/caddy-revproxy
