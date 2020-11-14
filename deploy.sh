00552267ee659c1f2d6b4e82720263074c0210f7
Olav Aasgaard <olav.aasgaard@siemens-healthineers.com>
2020-11-14 16:40:15 +0100

    Added delete all images in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash    
+# docker rm -vf $(docker ps -a -q)
+# docker rmi -f $(docker images -a -q)
 docker build --tag oaasgaa01/caddy-revproxy --no-cache .
 docker stop revproxy
 docker run -d --rm --name revproxy -p 80:80 -p 443:443 -v revproxy_data:/data -v revproxy_config:/config oaasgaa01/caddy-revproxy
